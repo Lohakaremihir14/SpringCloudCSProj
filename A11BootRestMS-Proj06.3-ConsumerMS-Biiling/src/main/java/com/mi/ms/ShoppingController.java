670224f3ceb88813dfc01629f35c25b90378fa2a
Mihir Lohakare <117346340+Lohakaremihir14@users.noreply.github.com>
2025-05-20 12:23:13 +0530

    added actuator

--- A11BootRestMS-Proj06.3-ConsumerMS-Biiling/src/main/java/com/mi/ms/ShoppingController.java
+++ A11BootRestMS-Proj06.3-ConsumerMS-Biiling/src/main/java/com/mi/ms/ShoppingController.java
@@ -2,6 +2,7 @@ package com.mi.ms;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import com.mi.client.IBillingServiceRestConsumer;
 
 @RestController
 @RequestMapping("/shopping/api")
+@RefreshScope
 public class ShoppingController 
 {
 	@Autowired
